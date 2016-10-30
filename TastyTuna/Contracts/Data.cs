9bfd374c28db21560925ef926781602421e330ce
Werner van der Riet <AKA.Werner@Gmail.com>
2016-10-30 22:45:28 +0200

    Basic setup, cannot seem to pass credentials

---
+++ TastyTuna/Contracts/Data.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace TastyTuna.Contracts
+{
+    public class Data
+    {
+        [DataContract]
+        public class Response
+        {
+            [DataMember(Name = "id")]
+            public string Id { get; set; }
+            [DataMember(Name = "name")]
+            public string Name { get; set; }
+            [DataMember(Name = "customersWaiting")]
+            public int CustomersWaiting { get; set; }
+            [DataMember(Name = "openServicePoints")]
+            public string OpenServicePoints { get; set; }
+            [DataMember(Name = "maxWaitingTime")]
+            public string MaxWaitingTime { get; set; }
+            [DataMember(Name = "averageWaitingTime")]
+            public string[] AverageWaitingTime { get; set; }
+            [DataMember(Name = "totalWaitingTime")]
+            public string TotalWaitingTime { get; set; }
+            [DataMember(Name = "customersBeingServed")]
+            public string CustomersBeingServed { get; set; }
+            [DataMember(Name = "totalWaitingTime")]            
+            public Parameters[] Parameters { get; set; }
+        }
+
+
+        [DataContract]
+        public class Parameters
+        {
+            [DataMember(Name = "description")]
+            public string Description { get; set; }
+            [DataMember(Name = "address1")]
+            public string Address1 { get; set; }
+            [DataMember(Name = "address2")]
+            public string Address2 { get; set; }
+            [DataMember(Name = "longitude")]
+            public long Longitude { get; set; }
+            [DataMember(Name = "latitude")]
+            public long Latitude { get; set; }
+            [DataMember(Name = "address3")]
+            public long Address3 { get; set; }
+            [DataMember(Name = "reset.time")]
+            public long ResetTime { get; set; }
+            [DataMember(Name = "postcode")]
+            public long postcode { get; set; }
+            [DataMember(Name = "city")]
+            public long City { get; set; }
+            [DataMember(Name = "country")]
+            public long Country { get; set; }
+        }
+
+    }
+}
---
+++ TastyTuna/Helper_Classes/Requests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Web;
+using System.Collections.Generic;
+using TastyTuna.Contracts;
+
+namespace TastyTuna.Helper_Classes
+{
+    public class Requests
+    {
+        public string CreateRequest(string queryString)
+        {
+            string UrlRequest = "http://orchestra.emergea.mu:8080/rest/managementinformation/branches";
+            return (UrlRequest);
+        }
+
+        public Data.Response MakeRequest(string requestUrl)
+        {            
+            HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
+            request.Credentials = new NetworkCredential("Administrator", "n-63M!E)-w");
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                    throw new Exception(String.Format(
+                    "Server error (HTTP {0}: {1}).",
+                    response.StatusCode,
+                    response.StatusDescription));
+                DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Data.Response));
+                object objResponse = jsonSerializer.ReadObject(response.GetResponseStream());
+                Data.Response jsonResponse
+                = objResponse as Data.Response;
+                return jsonResponse;
+            }            
+        }
+
+        public void ProcessResponse(Data.Response BranchesResponse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
